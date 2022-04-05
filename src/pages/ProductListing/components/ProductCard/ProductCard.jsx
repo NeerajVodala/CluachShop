e84da6af749fc22cec4f6dab077144b86bac087a
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-04-05 22:59:06 +0530

    fetch & render data using axios

---
+++ src/pages/ProductListing/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,42 @@
+export const ProductCard = ({ productDetail }) => {
+  const {
+    _id,
+    title,
+    collection,
+    price,
+    discount,
+    discountPrice,
+    imageUrl,
+    fastDelivery,
+    inStock,
+  } = productDetail;
+
+  return (
+    <>
+      <div className="card card-vertical flex-col shadow-1 br-s">
+        <div className="card-media">
+          <img src={imageUrl} alt="card media" />
+        </div>
+        <div className="card-badge">{discount}% off</div>
+        <div className="card-nonmedia">
+          <div className="card-details flex-col">
+            <p className="text-center text-l">{title}</p>
+            <p className="text-center text-s">{collection}</p>
+            <p className="text-center text-bold text-xl">
+              ₹{discountPrice}{" "}
+              <span className="text-m text-regular text-strike">₹{price}</span>
+            </p>
+          </div>
+          <div className="card-actions">
+            <button className="btn btn-m btn-fill br-s solid">
+              Add to Cart
+            </button>
+            <button className="btn btn-m btn-fill br-s outline">
+              Save to Wishlist
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
