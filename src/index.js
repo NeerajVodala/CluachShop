9e69963354f74d160ec1e19644e8c275a6f200a3
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-04-07 02:09:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { FiltersProvider } from "./contexts/filter-context";
-import { ProductsProvider } from "./contexts/product-context";
+import { FiltersProvider, ProductsProvider } from "./contexts";
 import { makeServer } from "./server";
 
 // Call make Server
