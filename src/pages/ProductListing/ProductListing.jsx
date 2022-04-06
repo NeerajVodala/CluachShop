562533858c5a9889fbfb9bdcd0c42c8783ebc5e3
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-04-06 06:28:41 +0530

    moved useEffect

--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,19 +1,9 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { ProductCard } from "./components/ProductCard/ProductCard";
 import { ProductFilters } from "./components/ProductFilters/ProductsFilters";
 import "./ProductListing.css";
 
 export const ProductListing = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get("/api/products");
-      setProducts(response.data.products);
-    })();
-  }, []);
-
   return (
     <>
       <Navbar />
